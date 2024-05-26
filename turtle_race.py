1c4be080004d22320783ac57fa987e40376f065d
felix <61442272+felix-prabowo@users.noreply.github.com>
2024-05-26 18:20:48 +0700

    Set up the race and declare a winner

--- turtle_race.py
+++ turtle_race.py
@@ -1,6 +1,9 @@
 import turtle
+import time
+import random
 
 WIDTH, HEIGHT = 500, 500
+COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
 
 def get_number_of_racers():
     racers = 0
@@ -17,6 +20,33 @@ def get_number_of_racers():
         else:
             print('Number not in range 2-10. Try Again!')
 
+def race(colors):
+    turtles = create_turtles(colors)
+
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 20:
+                return colors[turtles.index(racer)]
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+        
+    return turtles
+
 def init_turtle():
     screen = turtle.Screen()
     screen.setup(WIDTH, HEIGHT)
@@ -24,3 +54,10 @@ def init_turtle():
 
 racers = get_number_of_racers()
 init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+
+winner = race(colors)
+print("The winner is the turtle with color:", winner)
+time.sleep(5)
