804275d5ee1ca27b916335a58b1209d83e176338
felix <61442272+felix-prabowo@users.noreply.github.com>
2024-05-26 15:06:37 +0700

    Get user input for # of racers

---
+++ turtle_race.py
@@ -0,0 +1,26 @@
+import turtle
+
+WIDTH, HEIGHT = 500, 500
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input('Enter the number of racers (2 - 10): ')
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print('Input is not numeric... Try Again!')
+            continue
+
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print('Number not in range 2-10. Try Again!')
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title('Turtle Racing!')
+
+racers = get_number_of_racers()
+init_turtle()
